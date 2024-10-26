c025944f27dc304001a506930461b462a71ffdc4
janker6 <janker6@gmail.com>
2024-10-26 17:39:43 +0200

    adjustment in: GlobalExceptionHandler and TodoFrontendDto

--- src/main/java/org/example/todo_application/dto/TodoFrontendDto.java
+++ src/main/java/org/example/todo_application/dto/TodoFrontendDto.java
@@ -1,10 +1,14 @@
 package org.example.todo_application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoFrontendDto {
     private Long todoId;
     private String name;
